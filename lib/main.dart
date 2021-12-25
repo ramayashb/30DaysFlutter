b16499500dc78e6f5cf97d92e1be8fdef0782100
ramayashb <ramayash.bhumca2013@gmail.com>
2021-12-25 08:08:55 +0530

    day  10 show list by model

--- lib/main.dart
+++ lib/main.dart
@@ -29,3 +29,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// return  MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//     //  home: HomePage(),
+//      themeMode: ThemeMode.light,
+//      theme: MyTheme.lightTheme(context), 
+//      darkTheme: MyTheme.darkTheme(context),
+//      initialRoute: MyRoutes.homeRoute,
+//      routes: {
+//        "/" : (context)=> HomePage(),
+//        MyRoutes.homeRoute: (context) => HomePage(),
+//        MyRoutes.loginRoute: (context) => LoginPage(),
+//      },
+//     )
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_demo/models/catalog.dart';
 import 'package:flutter_application_demo/widgets/drawer.dart';
+import 'package:flutter_application_demo/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     int days = 30;
-
+    //final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catelog App"),
       ),
-      body: Center(child: Container(child: Text("hi $days"))),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: CatalogModel.items[index],);
+            },
+          ),
+      ),
       drawer: MyDrawer(),
     );
   }
