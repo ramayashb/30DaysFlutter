c8cbc31e2b297c6f5e2e653610d5fe3828d50d72
ramayashb <ramayash.bhumca2013@gmail.com>
2021-12-27 11:49:58 +0530

    Day 13 Create initial Grid View

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -24,9 +25,8 @@ class _HomePageState extends State<HomePage> {
     var catlogJson = await rootBundle.loadString("assets/files/catalog.json");
     var decodeData = jsonDecode(catlogJson);
     var productData = decodeData["products"];
-    CatalogModel.items = List.from(productData)
-    .map<Item>((item) => Item.fromMap(item))
-    .toList();
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
     setState(() {});
   }
 
@@ -40,13 +40,53 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)? ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) => ItemWidget(
-              item: CatalogModel.items[index],
-            )
-          
-        ) : Center(child: CircularProgressIndicator(),),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ?
+            // ListView.builder(
+            //   itemCount: CatalogModel.items.length,
+            //   itemBuilder: (context, index) => ItemWidget(
+            //       item: CatalogModel.items[index],
+            //     )
+            // )
+            GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16,
+                    ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(item.name,
+                         style: TextStyle(color: Colors.white),
+                         ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple
+                        ),
+                        ),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        child: Text(
+                          '\$${item.price}',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        decoration: BoxDecoration(color: Colors.black),
+                      ),
+                      
+                      ),
+                  );
+                },
+                itemCount: CatalogModel.items.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
